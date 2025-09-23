2e0b435784209572654e3521a0bdcc815de29b21
Danial <127108898+DanialArjmand@users.noreply.github.com>
2025-09-24 02:41:04 +0330

    Add SearchBar Component

--- src/app/cars/page.js
+++ src/app/cars/page.js
@@ -1,10 +1,12 @@
 import Categories from "@/components/module/Categories";
-import CarsPage from "../../components/template/CarsPage";
-import carsData from "../../data/carsData";
+import CarsPage from "@/components/template/CarsPage";
+import carsData from "@/data/carsData";
+import SearchBar from "@/components/module/SearchBar";
 
 function Details() {
   return (
     <div>
+      <SearchBar />
       <Categories />
       <CarsPage data={carsData} />
     </div>
